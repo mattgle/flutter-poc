229f13c4d1257c6244f393af1f849abd3827a1fd
Matias <matias.grote@wolox.com.ar>
2019-11-11 11:34:13 -0300

    Fix tab position

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -7,24 +7,26 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(
-            'LIBRARY',
-            style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold),
-          ),
-          flexibleSpace: Image(
-            image: AssetImage('images/bc_nav_bar.png'),
-            fit: BoxFit.fitWidth,
-          ),
-          backgroundColor: Color(0xffeaf6f),
-          elevation: 0.0,
-          leading: IconButton(
-            icon: const ImageIcon(AssetImage('images/ic_notifications.png')),
-            tooltip: 'Alert',
-            onPressed: () {},
-          )),
-      body: BookList()
-    );
+        appBar: AppBar(
+            title: Text(
+              'LIBRARY',
+              style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold),
+            ),
+            flexibleSpace: Image(
+              image: AssetImage('images/bc_nav_bar.png'),
+              fit: BoxFit.fitWidth,
+            ),
+            backgroundColor: Color(0xffeaf6f),
+            elevation: 0.0,
+            leading: IconButton(
+              icon: const ImageIcon(AssetImage('images/ic_notifications.png')),
+              tooltip: 'Alert',
+              onPressed: () {},
+            )),
+        body: Stack(children: [
+          BookList(),
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[Tabs()],)
+        ]));
   }
 }
 
